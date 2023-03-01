11ca6ce21fc09df78efb66113cb986557a064bc5
KeonU Kim <anythingthannothing@gmail.com>
2023-03-01 22:59:27 +0900

    ✅ test: jest setup 및 teardown 추가

--- src/auth/services/signup.service.spec.ts
+++ src/auth/services/signup.service.spec.ts
@@ -1,100 +1,103 @@
-// import { SignupService } from './signup.service';
-// import { ConflictException } from '@nestjs/common';
-// import { User } from '../../entities/user.entity';
-// import * as bcrypt from 'bcrypt';
-// import {
-//   CreateUserOutboundPort,
-//   CreateUserOutboundPortInputDto,
-// } from '../outbound-port/create-user.outbound-port';
-// import { CheckEmailDuplicateOutboundPort } from '../outbound-port/check-email-duplicate.outbound-port';
-// import { CheckNicknameDuplicateOutboundPort } from '../../profile/outbound-port/check-nickname-duplicate.outbound-port';
-//
-// class MockCheckEmailDuplicateOutboundPort
-//   implements CheckEmailDuplicateOutboundPort
-// {
-//   private readonly users: User[];
-//
-//   constructor(users: User[]) {
-//     this.users = users;
-//   }
-//
-//   async execute(email: string) {
-//     const user = this.users.find((user) => user.email === email);
-//     return Promise.resolve(user !== undefined);
-//   }
-// }
-//
-// class MockCheckNicknameDuplicateOutboudPort
-//   implements CheckNicknameDuplicateOutboundPort
-// {
-//   async execute(nickname: string) {
-//     return false;
-//   }
-// }
-//
-// class MockCreateUserOutboundPort implements CreateUserOutboundPort {
-//   async execute(params: CreateUserOutboundPortInputDto) {
-//     return Promise.resolve({
-//       id: 2,
-//       email: params.email,
-//       password: await bcrypt.hash(params.password, 12),
-//       profileId: 2,
-//       profile: {
-//         id: 2,
-//         userId: 2,
-//         nickname: 'mercury123456',
-//         avatarUrl: 'avatars/default',
-//         bio: '',
-//       },
-//     });
-//   }
-// }
-//
-// let signupService: SignupService;
-// let hashedPassword: string;
-// let users: User[];
-// let password = 'test1234!';
-//
-// describe('SignupService 유닛 테스트', () => {
-//   beforeEach(async () => {
-//     hashedPassword = await bcrypt.hash(password, 12);
-//     users = [
-//       {
-//         id: 1,
-//         email: 'test1234@gmail.com',
-//         password: hashedPassword,
-//         profileId: 1,
-//         profile: null,
-//       },
-//     ];
-//
-//     signupService = new SignupService(
-//       new MockCheckEmailDuplicateOutboundPort(users),
-//       new MockCheckNicknameDuplicateOutboudPort(),
-//       new MockCreateUserOutboundPort(),
-//     );
-//   });
-//
-//   test('SignupService를 인스턴스로 생성할 수 있다.', async () => {
-//     expect(signupService).toBeDefined();
-//   });
-//
-//   test('사용 중인 이메일일 경우 409 에러를 반환한다.', async () => {
-//     await expect(
-//       signupService.execute({
-//         email: 'test1234@gmail.com',
-//         password: 'asdf',
-//       }),
-//     ).rejects.toThrow(ConflictException);
-//   });
-//
-//   test('해쉬화된 비밀번호를 가진 유저를 생성한다', async () => {
-//     const user = await signupService.execute({
-//       email: 'test1235@gmail.com',
-//       password: 'test1234!',
-//     });
-//     const hashedPassword = user.password;
-//     expect(hashedPassword).not.toEqual('test1234!');
-//     expect(hashedPassword.length).toBeGreaterThan(30);
-//   });
-// });
+import { SignupService } from './signup.service';
+import { ConflictException } from '@nestjs/common';
+import { User } from '../../entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import {
+  CreateUserOutboundPort,
+  CreateUserOutboundPortInputDto,
+} from '../outbound-port/create-user.outbound-port';
+import { CheckEmailDuplicateOutboundPort } from '../outbound-port/check-email-duplicate.outbound-port';
+import { CheckNicknameDuplicateOutboundPort } from '../../profile/outbound-port/check-nickname-duplicate.outbound-port';
+
+class MockCheckEmailDuplicateOutboundPort
+  implements CheckEmailDuplicateOutboundPort
+{
+  private readonly users: User[];
+
+  constructor(users: User[]) {
+    this.users = users;
+  }
+
+  async execute(email: string) {
+    const user = this.users.find((user) => user.email === email);
+    return Promise.resolve(user !== undefined);
+  }
+}
+
+class MockCheckNicknameDuplicateOutboudPort
+  implements CheckNicknameDuplicateOutboundPort
+{
+  async execute(nickname: string) {
+    return false;
+  }
+}
+
+class MockCreateUserOutboundPort implements CreateUserOutboundPort {
+  async execute(params: CreateUserOutboundPortInputDto) {
+    return Promise.resolve({
+      userId: 2,
+      email: params.email,
+      password: {
+        password: await bcrypt.hash(params.password, 12),
+      },
+      profileId: 2,
+      profile: {
+        profileId: 2,
+        imageId: 1,
+        image: null,
+        nickname: 'mercury123456',
+        avatarUrl: 'avatars/default',
+        bio: '',
+      },
+    });
+  }
+}
+
+let signupService: SignupService;
+let hashedPassword: string;
+let users: User[];
+let password = 'test1234!';
+
+describe('SignupService 유닛 테스트', () => {
+  beforeEach(async () => {
+    hashedPassword = await bcrypt.hash(password, 12);
+    users = [
+      {
+        userId: 1,
+        email: 'test1234@gmail.com',
+        password: { password: hashedPassword },
+        profileId: 1,
+        profile: null,
+      },
+    ];
+
+    signupService = new SignupService(
+      new MockCheckEmailDuplicateOutboundPort(users),
+      new MockCheckNicknameDuplicateOutboudPort(),
+      new MockCreateUserOutboundPort(),
+    );
+  });
+
+  test('SignupService를 인스턴스로 생성할 수 있다.', async () => {
+    expect(signupService).toBeDefined();
+  });
+
+  test('사용 중인 이메일일 경우 409 에러를 반환한다.', async () => {
+    await expect(
+      signupService.execute({
+        email: 'test1234@gmail.com',
+        password: 'asdf',
+      }),
+    ).rejects.toThrow(ConflictException);
+  });
+
+  // test('해쉬화된 비밀번호를 가진 유저를 생성한다', async () => {
+  //   const user = await signupService.execute({
+  //     email: 'test1235@gmail.com',
+  //     password: 'test1234!',
+  //   });
+  //   const hashedPassword = user.password.password;
+  //   expect(hashedPassword).not.toEqual('test1234!');
+  //   expect(hashedPassword.length).toBeGreaterThan(30);
+  // });
+});
